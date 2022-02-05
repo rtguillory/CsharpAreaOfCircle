fd0481fe308582ac5a79a94fd0829bedc59e2dfc
Rhonda Guillory <guilloryrt.ps@gmail.com>
2022-02-04 23:36:51 -0600

    added comments and try-parse

--- CsharpAreaOfCircle/Program.cs
+++ CsharpAreaOfCircle/Program.cs
@@ -1,8 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+//Operators Exercise 2 -- Area of Circle
+//User input to get radius
 Console.WriteLine("What is the radius of your circle?");
-var radius = Console.ReadLine();
-double rad = Convert.ToDouble(radius);
-Console.WriteLine($"The area of a circle with radius {rad} is {AreaOfCircle(rad)}");
+
+double radius;  //declared for use as out parameter
+
+//handles string inputs--assigns 0 radius for string
+//assigns input to radius if input is a number
+var radiusInput = double.TryParse(Console.ReadLine(), out radius);  //handles string inputs--assigns 0 radius
+
+Console.WriteLine($"The area of a circle with radius {radius} is {AreaOfCircle(radius)}");
 
 
 
