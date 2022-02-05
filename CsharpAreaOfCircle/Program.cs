0e1c70dced48ef8957afa95dc0f4ec77242e4024
Rhonda Guillory <guilloryrt.ps@gmail.com>
2022-02-04 23:21:34 -0600

    initial commit

---
+++ CsharpAreaOfCircle/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is the radius of your circle?");
+var radius = Console.ReadLine();
+double rad = Convert.ToDouble(radius);
+Console.WriteLine($"The area of a circle with radius {rad} is {AreaOfCircle(rad)}");
+
+
+
+static double AreaOfCircle(double radius)
+{
+    return Math.PI * Math.Pow(radius, 2);
+}
